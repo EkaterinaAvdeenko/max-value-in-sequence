bc8ad1c97e5aa1a248014c0ccf4b9df5e8f22cbc
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-12-17 19:11:14 +0100

    max-in-seq_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -1,29 +1,32 @@
 package com.epam.rd.autotasks.sequence;
+
 import java.util.Scanner;
 
 public class FindMaxInSeq {
 
-        public static int max() {
-            Scanner scan = new Scanner(System.in);
-            int currValue = scan.nextInt();
-            int maxSoFar=currValue;
-            while (true) {
-                currValue = scan.nextInt();
-                if(currValue==0)
-                    break;
-                if (currValue >= maxSoFar ) {
-                    maxSoFar = currValue;
-                }
-            }
-            return maxSoFar;
+  public static int max() {
+    Scanner scan = new Scanner(System.in);
+    int currValue = scan.nextInt();
+    int maxSoFar = currValue;
+    while (true) {
+      currValue = scan.nextInt();
+        if (currValue == 0) {
+            break;
         }
-        public static void main (String[]args){
-
-            System.out.println("Test your code here!\n");
-
-            FindMaxInSeq test = new FindMaxInSeq();
-
-            System.out.println(max());
-
+      if (currValue >= maxSoFar) {
+        maxSoFar = currValue;
+      }
     }
+    return maxSoFar;
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println("Test your code here!\n");
+
+    FindMaxInSeq test = new FindMaxInSeq();
+
+    System.out.println(max());
+
+  }
 }
